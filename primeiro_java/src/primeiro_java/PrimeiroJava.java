0a36f2145c1b358aca6a2baa3b11023c09954f65
digao00 <joaorodrigogame@gmail.com>
2024-10-03 17:54:27 -0400

    criando metodos

--- primeiro_java/src/primeiro_java/PrimeiroJava.java
+++ primeiro_java/src/primeiro_java/PrimeiroJava.java
@@ -1,9 +1,48 @@
 package primeiro_java;
 
-public class PrimeiroJava {
+import java.util.Scanner;
 
+public class PrimeiroJava {
+    
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        /*/somar tres num
+        System.out.print("Digite 3 numeros\n->");
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c = read.nextInt();
+        System.out.println("soma = " + somarTres(a,b,c));
+        //ver se é par 
+        System.out.println("Digite um numero: ");
+        int x = read.nextInt();
+        if(seÉPar(x)) {
+            System.out.println("É par");
+        }
+        else {
+            System.out.println("É impar");
+        }*/
+        //dividir dois reais
+        float y = read.nextFloat();
+        float z = read.nextFloat();
+        System.out.println(dividirDois(y,z));
         
     }
-    
+    public static float dividirDois(float a,float b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
+    public static boolean seÉPar(int a) {
+        //verifica se o numero "a" é par ou não
+        return a % 2 == 0;
+    }
+    public static int somarDois(int a, int b) {
+        // soma dois valores
+        return a + b;
+    }
+    public static int somarTres(int a, int b, int c) {
+        // tecla do "mais" quebrou aqui
+        return somarDois(somarDois(a,b),c);
+    }
 }
