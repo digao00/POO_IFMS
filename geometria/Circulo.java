2efd8351f79bf7778655177016a4f904258d62f2
digao00 <joaorodrigogame@gmail.com>
2024-11-07 14:56:12 -0400

    circulo

---
+++ geometria/Circulo.java
@@ -0,0 +1,40 @@
+package geometria;
+
+public class Circulo {
+    private Ponto centro;
+    private float raio;
+
+    public Circulo(Ponto centro, float raio) {
+        this.centro = centro;
+        this.raio = raio;
+    }
+
+    public float area() {
+        return (float)Math.PI*(raio*raio);
+    }
+    public boolean intercecta(Circulo circulo) {
+        if (centro.distancia(this.centro) < raio + this.raio) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void print() {
+        System.out.printf("\nRaio: %.2f\nCentro: (%.2f,%.2f)", raio, centro.getX(), centro.getY());
+    }
+
+    public Ponto getCentro() {
+        return centro;
+    }
+    public void setCentro(Ponto centro) {
+        this.centro = centro;
+    }
+    
+    public float getRaio() {
+        return raio;
+    }
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+}
--- geometria/Main.java
+++ geometria/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Ponto a = new Ponto(3f,3f);
         Ponto b = new Ponto(2f,2f);
+        Circulo c1 = new Circulo(a, 5);
+        Circulo c2 = new Circulo(b, 3);
         Retangulo r = new Retangulo(a, b);
         float d = a.distancia(b);
         a.print("a");
@@ -11,5 +13,6 @@ public class Main {
         System.out.println("\nDistancia entre A e B: " + d);
         System.out.println("Area do retangulo: " + r.area());
         System.out.println("Perimetro do retangulo: " + r.perimetro());
+        System.out.println(c1.intercecta(c2));
     }
 }
