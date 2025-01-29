7ec17d19c468a989969914ef56807fafb9ffaad6
digao00 <joaorodrigogame@gmail.com>
2025-01-29 09:39:38 -0400

    escrever disciplinas

--- Escola/Disciplina.java
+++ Escola/Disciplina.java
@@ -1,5 +1,55 @@
 package Escola;
 
+import java.util.ArrayList;
+
 public class Disciplina {
+    private String nome;
+    private String prof;
+    private int numEstudantes;
+    private ArrayList<Estudante> listaEstudantes;
     
+    public Disciplina(String nome, String prof, int numEstudantes) {
+        this.nome = nome;
+        this.prof = prof;
+        this.numEstudantes = numEstudantes;
+    }
+
+    public ArrayList<Estudante> getListaEstudantes() {
+        return listaEstudantes;
+    }
+    public void setListaEstudantes(ArrayList<Estudante> listaEstudantes) {
+        this.listaEstudantes = listaEstudantes;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getProf() {
+        return prof;
+    }
+    public void setProf(String prof) {
+        this.prof = prof;
+    }
+
+    public int getNumEstudantes() {
+        return numEstudantes;
+    }
+    public void setNumEstudantes(int numEstudantes) {
+        this.numEstudantes = numEstudantes;
+    }
+
+    public void addEstudante(Estudante estudante) {
+        listaEstudantes.add(estudante);
+    }
+
+    public void print() {
+        System.out.printf("\nDisciplina: %s\nProf: %s\nNumero de Estudantes: %d", nome, prof, numEstudantes);
+        for (Estudante estudante : listaEstudantes) {
+            estudante.print();
+        }
+    }
 }
--- Escola/Main.java
+++ Escola/Main.java
@@ -8,12 +8,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Estudante> listaEstudantes = new ArrayList<>();
-        readEstudantes(listaEstudantes, "C:\\Users\\aluno\\Desktop\\a\\Escola\\teste.txt");
-        for (Estudante estudante : listaEstudantes) {
-            estudante.print();
+        ArrayList<Disciplina> listaDisciplinas = new ArrayList<>();
+        readDisciplina(listaDisciplinas, "C:\\Users\\joaor\\OneDrive\\Desktop\\jao_tarefa\\Escola\\teste.txt");
+        for (Disciplina disciplina : listaDisciplinas) {
+            disciplina.print();
         }
-        writeEstudantes(listaEstudantes, "C:\\Users\\aluno\\Desktop\\a\\Escola\\teste.txt");
+        Disciplina d1 = new Disciplina("Matemática", "Elton", 2);
+        d1.addEstudante(new Estudante("84392", "João Rodrigo", (short)17));
+        d1.addEstudante(new Estudante("35756", "Thiago", (short)16));
+        listaDisciplinas.add(d1);
+        writeDisciplinas(listaDisciplinas, "C:\\Users\\joaor\\OneDrive\\Desktop\\jao_tarefa\\Escola\\teste.txt");
     }
 
     //apaga o conteúdo e escreve os estudantes da lista atual no arquivo txt
@@ -43,7 +47,6 @@ public class Main {
         Scanner sc = null;
         try {
             sc = new Scanner(file);
-
             while (sc.hasNextLine()) {
                 String[] info = sc.nextLine().split("#");
                 lista.add(new Estudante(info[0], info[1], Short.valueOf(info[2])));    
@@ -67,5 +70,63 @@ public class Main {
             
         }
     }
-    //criar o método "readDiciplina"
+
+    //pega as disciplinas e alunos do arquivo e adciona
+    public static void readDisciplina(ArrayList<Disciplina> lista, String arquivoNome) {
+        File file = new File(arquivoNome);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String[] info = sc.nextLine().split("#");
+                Disciplina d = new Disciplina(info[0], info[1], Integer.valueOf(info[2]));
+                lista.add(d);
+                for(int i = 0; i < d.getNumEstudantes(); i++) {
+                    String[] infoEstudante = sc.nextLine().split("#");
+                    Estudante e = new Estudante(infoEstudante[0], infoEstudante[1], Short.valueOf(infoEstudante[2]));
+                    d.addEstudante(e);
+                }
+            }
+
+        } 
+        catch (FileNotFoundException e) {
+            System.out.printf("\nErro: Arquivo não encontrado\n%s", e.getMessage());
+
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.printf("\nErro: %s", e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+            
+        }
+    }
+    public static void writeDisciplinas(ArrayList<Disciplina> lista, String arquivoNome) {
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(arquivoNome);
+            for (Disciplina disciplina : lista) {
+                pw.println(disciplina.getNome() + "#" + disciplina.getProf() + "#" + disciplina.getNumEstudantes());
+                for (Estudante estudante : disciplina.getListaEstudantes()) {
+                    pw.println(estudante.getCpf() + "#" + estudante.getNome() + "#" + estudante.getIdade());
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.printf("\nErro: %s", e.getMessage());
+            
+        }
+        finally {
+            if (pw != null) {
+                pw.close();    
+            }
+            
+        }
+    }
+
 }
