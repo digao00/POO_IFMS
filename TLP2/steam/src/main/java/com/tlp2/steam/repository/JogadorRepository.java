ba9f0075ed0bc397400e516caaf91ffa9d16ed5a
digao00 <joaorodrigogame@gmail.com>
2025-09-08 17:54:38 -0400

    bootstrap agora

--- TLP2/steam/src/main/java/com/tlp2/steam/repository/JogadorRepository.java
+++ TLP2/steam/src/main/java/com/tlp2/steam/repository/JogadorRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tlp2.steam.model.Jogador;
 
 public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-    
+    @Override
+    default <S extends Jogador> S save(S entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
 }
