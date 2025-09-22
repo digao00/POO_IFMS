2ebfd0332fff28a4ec704c087df0d76e3ac7976e
digao00 <joaorodrigogame@gmail.com>
2025-09-22 17:46:31 -0400

    puta queo pariu viado

--- TLP2/steam/src/main/java/com/tlp2/steam/controller/JogadorController.java
+++ TLP2/steam/src/main/java/com/tlp2/steam/controller/JogadorController.java
@@ -3,12 +3,29 @@ package com.tlp2.steam.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class JogadorController {
 
     @GetMapping("/")
     public String cadastro() {
-        return "/cadastro";
+        return "/lista";
     }
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "/index";
+    }
+
+    @GetMapping("/novo")
+    public String novoJogador() {
+        return "/novoJogador";
+    } 
+    /*
+    @GetMapping("/qualquer coisa")
+    public String teste() {
+        return "/qualquer página";
+    }
+     */
 
 }
