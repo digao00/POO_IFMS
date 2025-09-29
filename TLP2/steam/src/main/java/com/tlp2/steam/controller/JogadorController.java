947d7bc390a1ab0c75e75592ff02781dcc76c359
digao00 <joaorodrigogame@gmail.com>
2025-09-29 17:39:22 -0400

    ai caralho

--- TLP2/steam/src/main/java/com/tlp2/steam/controller/JogadorController.java
+++ TLP2/steam/src/main/java/com/tlp2/steam/controller/JogadorController.java
@@ -1,15 +1,27 @@
 package com.tlp2.steam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.tlp2.steam.model.Jogador;
+import com.tlp2.steam.service.JogadorService;
+
 
 
 @Controller
 public class JogadorController {
 
+    @Autowired
+    private JogadorService jogadorService;
+
     @GetMapping("/")
     public String cadastro() {
-        return "/lista";
+        return "/jogador";
     }
 
     @GetMapping("/teste")
@@ -18,9 +30,19 @@ public class JogadorController {
     }
 
     @GetMapping("/novo")
-    public String novoJogador() {
-        return "/novoJogador";
-    } 
+    public String novoJogador(Model model) {
+        Jogador jogador = new Jogador();
+        model.addAttribute("novoJogador", jogador);
+        return "/index";
+    }
+    
+    @PostMapping("/gravar")
+    public String gravarEstudante(@ModelAttribute("novoJogador") Jogador jogador, RedirectAttributes attributes) {		
+		jogadorService.criarJogador(jogador);
+		attributes.addFlashAttribute("mensagem", "Conta criada com sucesso!");
+        return "redirect:/novo";
+    }
+    
     /*
     @GetMapping("/qualquer coisa")
     public String teste() {
