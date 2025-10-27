7a6a72716a24b5083f94010ac2b21b1ce861888f
digao00 <joaorodrigogame@gmail.com>
2025-10-27 17:50:17 -0400

    validação

--- TLP2/steam/src/main/java/com/tlp2/steam/controller/SteamController.java
+++ TLP2/steam/src/main/java/com/tlp2/steam/controller/SteamController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,35 +16,40 @@ import com.tlp2.steam.model.Jogo;
 import com.tlp2.steam.service.JogadorService;
 import com.tlp2.steam.service.JogoService;
 
+import jakarta.validation.Valid;
+
 
 
 @Controller
-public class SteamController {                                                                                       //I cannot vanish, you will not scare me
+public class SteamController {
 
-    @Autowired                                                                                                       //Try to get through it, try to push through it
-    private JogadorService jogadorService;                                                                           //You were not thinking that I will not do it
-    @Autowired                                                                                                       //They be lovin' someone and I'm another story
-    private JogoService jogoService;                                                                                 //Take the next ticket, get the next train
+    @Autowired
+    private JogadorService jogadorService;
+    @Autowired
+    private JogoService jogoService;
 
-    @GetMapping("/loja")                                                                                             //Why would I do it? Anyone'd think that
-    public String principal(Model model) {                                                                           //I cannot vanish, you will not scare me
-        List<Jogo> jogos = jogoService.listarJogos();                                                                //Try to get through it, try to push through it
-        model.addAttribute("jogos", jogos);                                                                          //You were not thinking that I will not do it
-        return "/main_page/index";                                                                                   //They be lovin' someone and I'm another story
-    }                                                                                                                //Take the next ticket, get the next train
+    @GetMapping("/loja")
+    public String principal(Model model) {
+        List<Jogo> jogos = jogoService.listarJogos();
+        model.addAttribute("jogos", jogos);
+        return "/main_page/index";
+    }
 
-    @GetMapping("/cadastro")                                                                                         //Why would I do it? Anyone'd think that
-    public String cadastro(Model model) {                                                                            //Baby, now I'm ready, moving on
-        Jogador jogador = new Jogador();                                                                             //Oh, but maybe I was ready all along
-        model.addAttribute("novoJogador", jogador);                                                                  //Oh, I'm ready for the moment and the sound
-        return "/cadastro";                                                                                          //Oh, but maybe I was ready all along
+    @GetMapping("/cadastro")
+    public String cadastro(Model model) {
+        Jogador jogador = new Jogador();
+        model.addAttribute("novoJogador", jogador);
+        return "/cadastro";
     }
     
-    @PostMapping("/gravar")                                                                                          //Why would I do it? Anyone'd think that
-    public String gravarJogador(@ModelAttribute("novoJogador") Jogador jogador, RedirectAttributes attributes) {     //Baby, now I'm ready, moving on
-		jogadorService.criarJogador(jogador);                                                                        //Oh, but maybe I was ready all along
-		attributes.addFlashAttribute("mensagem", "Conta criada com sucesso!");                                       //Oh, I'm ready for the moment and the sound
-        return "redirect:/loja";                                                                                     //Oh, but maybe I was ready all along
+    @PostMapping("/gravar")
+    public String gravarJogador(@ModelAttribute("novoJogador") @Valid Jogador jogador, BindingResult error, RedirectAttributes attributes) {
+        if (error.hasErrors()) {
+            return "/cadastro";
+        }
+		jogadorService.criarJogador(jogador);
+		attributes.addFlashAttribute("mensagem", "Conta criada com sucesso!");
+        return "redirect:/loja";
     }
 
 }
